a465189c5d3fa3bf5e881f62f61ef38c93c5956f
Gabodev <ga.espinozae@gmail.com>
2022-04-05 00:44:48 -0400

    se agrega script sql que inserta datos iniciales

---
+++ Sql/Insert_Tables.sql
@@ -0,0 +1,15 @@
+USE [Tarea]
+GO
+
+INSERT INTO [dbo].[Dron] VALUES ('AAA001', 1, 10, 50, 1)
+INSERT INTO [dbo].[Dron] VALUES ('AAA002', 2, 20, 50, 2)
+INSERT INTO [dbo].[Dron] VALUES ('AAA003', 3, 30, 100, 3)
+INSERT INTO [dbo].[Dron] VALUES ('AAA004', 4, 40, 100, 4)
+GO
+
+INSERT INTO [dbo].[Medicamento] VALUES ('PARACETAMOL', 1, 'CCC0001', '')
+INSERT INTO [dbo].[Medicamento] VALUES ('IBUPROFENNO', 2, 'CCC0002', '')
+INSERT INTO [dbo].[Medicamento] VALUES ('ASPIRINA', 3, 'CCC0003', '')
+INSERT INTO [dbo].[Medicamento] VALUES ('OMEPRAZOL', 4, 'CCC0004', '')
+GO
+
